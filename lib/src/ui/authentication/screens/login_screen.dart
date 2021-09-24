40a6608bbd4283155d0f66e9584ba5bc740bba80
lambiengcode <lambiengcode@gmail.com>
2021-09-24 20:44:00 +0700

    feat(Login): obscure text password

--- lib/src/ui/authentication/screens/login_screen.dart
+++ lib/src/ui/authentication/screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:cloudmate/src/themes/font_family.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:sizer/sizer.dart';
 
 class LoginPage extends StatefulWidget {
@@ -43,122 +44,127 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children: [
                 Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SizedBox(height: 12.sp),
-                        Container(
-                          height: 42.h,
-                          width: 100.w,
-                          margin: EdgeInsets.fromLTRB(
-                            10.w,
-                            8.8.h - 12.sp,
-                            10.w,
-                            12.sp,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(height: 12.sp),
+                          Container(
+                            height: 42.h,
+                            width: 100.w,
+                            margin: EdgeInsets.fromLTRB(
+                              10.w,
+                              8.8.h - 12.sp,
+                              10.w,
+                              12.sp,
+                            ),
+                            child: Lottie.asset('assets/lottie/splash.json'),
                           ),
-                          child: Lottie.asset('assets/lottie/splash.json'),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 10.w,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(
-                                decoration: AppDecoration.buttonActionBorder(
-                                        context, 6.sp)
-                                    .decoration,
-                                child: Column(
-                                  children: <Widget>[
-                                    SizedBox(height: 10.0),
-                                    _buildLineInfo(
-                                      context,
-                                      'Email',
-                                      'Hãy nhập email của bạn',
-                                      usernameFocus,
-                                    ),
-                                    Divider(
-                                      thickness: .25,
-                                      height: .25,
-                                      indent: 16.sp,
-                                      endIndent: 16.sp,
-                                    ),
-                                    _buildLineInfo(
-                                      context,
-                                      'Mật khẩu',
-                                      'Mật khẩu phải có tối thiểu 6 kí tự',
-                                      passwordFocus,
-                                    ),
-                                    SizedBox(height: 6.0),
-                                  ],
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  widget.toggleView!();
-                                },
-                                child: Container(
-                                  margin: EdgeInsets.only(
-                                    top: 18.sp,
-                                    bottom: 12.sp,
-                                  ),
-                                  alignment: Alignment.center,
-                                  child: RichText(
-                                    textAlign: TextAlign.center,
-                                    text: TextSpan(
-                                      style: TextStyle(
-                                        fontFamily: FontFamily.lato,
-                                        fontSize: 11.sp,
-                                        color: Theme.of(context)
-                                            .textTheme
-                                            .bodyText2!
-                                            .color,
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 10.w,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  decoration: AppDecoration.buttonActionBorder(
+                                    context,
+                                    6.sp,
+                                  ).decoration,
+                                  child: Column(
+                                    children: <Widget>[
+                                      SizedBox(height: 10.0),
+                                      _buildLineInfo(
+                                        context,
+                                        'Email',
+                                        'Hãy nhập email của bạn',
+                                        usernameFocus,
                                       ),
-                                      children: [
-                                        TextSpan(
-                                          text: 'Bạn chưa có tài khoản? ',
+                                      Divider(
+                                        thickness: .25,
+                                        height: .25,
+                                        indent: 16.sp,
+                                        endIndent: 16.sp,
+                                      ),
+                                      _buildLineInfo(
+                                        context,
+                                        'Mật khẩu',
+                                        'Mật khẩu phải có tối thiểu 6 kí tự',
+                                        passwordFocus,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    widget.toggleView!();
+                                  },
+                                  child: Container(
+                                    margin: EdgeInsets.only(
+                                      top: 18.sp,
+                                      bottom: 12.sp,
+                                    ),
+                                    alignment: Alignment.center,
+                                    child: RichText(
+                                      textAlign: TextAlign.center,
+                                      text: TextSpan(
+                                        style: TextStyle(
+                                          fontFamily: FontFamily.lato,
+                                          fontSize: 11.sp,
+                                          color: Theme.of(context)
+                                              .textTheme
+                                              .bodyText2!
+                                              .color,
                                         ),
-                                        TextSpan(
-                                          text: 'Đăng kí ngay',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w600,
+                                        children: [
+                                          TextSpan(
+                                            text: 'Bạn chưa có tài khoản? ',
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () async {},
-                                child: Container(
-                                  height: 40.sp,
-                                  margin:
-                                      EdgeInsets.symmetric(horizontal: 16.sp),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8.sp),
-                                    color: colorPrimary,
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      'Đăng nhập',
-                                      style: TextStyle(
-                                        color: mC,
-                                        fontSize: 10.sp,
-                                        fontFamily: FontFamily.lato,
-                                        fontWeight: FontWeight.w600,
+                                          TextSpan(
+                                            text: 'Đăng kí ngay',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                                SizedBox(height: 6.sp),
+                                GestureDetector(
+                                  onTap: () async {},
+                                  child: Container(
+                                    height: 40.sp,
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 16.sp),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8.sp),
+                                      color: colorPrimary,
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Đăng nhập',
+                                        style: TextStyle(
+                                          color: mC,
+                                          fontSize: 10.sp,
+                                          fontFamily: FontFamily.lato,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -183,7 +189,7 @@ class _LoginPageState extends State<LoginPage> {
     return Container(
       padding: EdgeInsets.all(8),
       child: TextFormField(
-        cursorColor: Theme.of(context).textTheme.bodyText2!.color,
+        cursorColor: Theme.of(context).primaryColor,
         focusNode: focusNode,
         onFieldSubmitted: (val) {
           usernameFocus.unfocus();
@@ -206,6 +212,7 @@ class _LoginPageState extends State<LoginPage> {
             password = val.trim();
           }
         },
+        obscureText: title == 'Mật khẩu' ? hidePassword : false,
         decoration: InputDecoration(
           floatingLabelBehavior: FloatingLabelBehavior.always,
           contentPadding: EdgeInsets.only(
@@ -219,6 +226,20 @@ class _LoginPageState extends State<LoginPage> {
             fontSize: 11.sp,
             fontWeight: FontWeight.w500,
           ),
+          suffixIcon: title == 'Mật khẩu'
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      hidePassword = !hidePassword;
+                    });
+                  },
+                  icon: Icon(
+                    hidePassword ? PhosphorIcons.eyeSlash : PhosphorIcons.eye,
+                    color: colorPrimary,
+                    size: 16.sp,
+                  ),
+                )
+              : null,
         ),
       ),
     );
